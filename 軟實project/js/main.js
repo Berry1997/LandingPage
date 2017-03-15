43d3bc95808e7d5ed047d07bed551e33fb77f331
ebobryan <bryan07270@gmail.com>
2017-03-15 12:36:03 +0800

    removed unnecessary files

--- 軟實project/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#ccd").click(function(){
-        $("#banner").velocity("scroll", {
-          container: $("body"),
-          duration: 800,
-          delay: 500
-        });
-    });
-});
